593f0fba1744244d9cd8936f24a2053ee687349a
egparrac <egparrac@estudiantes.uhemisferios.edu.ec>
2026-07-05 13:18:52 -0500

    Update App.py

--- App.py
+++ App.py
@@ -4,3 +4,12 @@ st.title("Aplicación de Fundamentos Streamlit")
 st.sidebar.image("LogoUHEpng.png")
 st.sidebar.title("Parámetros")
 
+
+
+
+monto = st.number_input("Ingrese el monto de su prestamo")
+interes = st.number_input("Ingrese el interes")
+tiempo_meses st.number_input("Ingrese el tiempo en meses")
+resultado = monto*interes*(tiempo_meses/12)
+
+st.write("El resultado es:", resultado)
