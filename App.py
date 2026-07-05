d89eb27cd88b288914b5a4e9383698ca14efd0d1
egparrac <egparrac@estudiantes.uhemisferios.edu.ec>
2026-07-05 13:22:09 -0500

    Update App.py

--- App.py
+++ App.py
@@ -7,7 +7,7 @@ st.sidebar.title("Parámetros")
 
 
 
-monto = st.number_input("Ingrese el monto de su prestamo")
+monto = st.number_input("Ingrese el monto de su prestamo", value = 5000)
 interes = st.number_input("Ingrese el interes")
 tiempo_meses = st.number_input("Ingrese el tiempo en meses")
 resultado = monto*interes*(tiempo_meses/12)
